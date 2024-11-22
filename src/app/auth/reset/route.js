c4928b4f0103e91de5932a9fe785718d0db9ba68
Joell Jones <25757151+joelljones@users.noreply.github.com>
2024-11-22 14:36:39 -0500

    refactor: update forgot password actions and reset password route

--- src/app/auth/reset/route.js
+++ src/app/auth/reset/route.js
@@ -3,19 +3,30 @@ import { NextResponse } from 'next/server'
 
 export async function GET(request) {
 	const { searchParams } = new URL(request.url)
-	const code = searchParams.get('code')
-	const next = searchParams.get('next') ?? '/'
+	const token_hash = searchParams.get('token_hash')
+	const type = searchParams.get('type')
+	const next = '/reset-password'
 
-	if (code) {
+	// Create redirect link without the secret token
+	const redirectTo = request.nextUrl.clone()
+	redirectTo.pathname = next
+	redirectTo.searchParams.delete('token_hash')
+	redirectTo.searchParams.delete('type')
+
+	if (token_hash && type) {
 		const supabase = createClient()
-		const { error } = await supabase.auth.exchangeCodeForSession(code)
+		const { error } = await supabase.auth.verifyOtp({
+			type,
+			token_hash,
+		})
 
-		if (error) {
-			return NextResponse.redirect(`${request.nextUrl.origin}/error`)
+		if (!error) {
+			redirectTo.searchParams.delete('next')
+			return NextResponse.redirect(redirectTo)
 		}
 	}
 
-	const redirectUrl = new URL(`${request.nextUrl.origin}${next}`)
-	redirectUrl.searchParams.set('type', 'recovery')
-	return NextResponse.redirect(redirectUrl)
+	// return the user to an error page with some instructions
+	redirectTo.pathname = '/error'
+	return NextResponse.redirect(redirectTo)
 }
